d31ac7d39f6412233f085e01bba1a5c11571ddad
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-10-30 11:06:07 +0100

    Add files via upload

--- cart-app/src/components/cart/ui.js
+++ cart-app/src/components/cart/ui.js
@@ -4,15 +4,33 @@ import { Money } from "../money/ui"
 import { Product } from "../product/ui"
 import { Quantity } from "./quantity"
 
-export function Cart( {item, quantity, cost }){
-  const items = getCartItems
+export function Cart() {
+  const items = getCartItems();
+
   return (
-    <tbody>
-      <tr>
-        <td>{(item = <Product />)}</td>
-        <td>{(cost = <Money />)}</td>
-        <td>{(quantity = <Quantity />)}</td>
-      </tr>
-    </tbody>
-  );
+    <table>
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Cost</th>
+          <th>Quantity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item, index) => (
+          <tr key={index}>
+            <td>
+              <Product productObject={item.product} />
+            </td>
+            <td>
+              <Money money={item.cost} />
+            </td>
+            <td>
+              <Quantity quantity={item.quantity} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
